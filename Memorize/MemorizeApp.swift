638f884de07a99546fb75aec504157c30cfb594b
SigridM <sigridm@selfhelpsoftware.com>
2022-08-12 15:24:49 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sigrid Mortensen on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
